4b6382f5a983842fa804b04bf3c9d2dda154535f
NoobManBr <mayconbj15@gmail.com>
2017-04-25 14:34:43 -0300

    Update prog62.c
    Mudança da constante, mudança da ordenação de vetores e mudança da função ter_maior

--- prog62.c
+++ prog62.c
@@ -8,118 +8,113 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define TAM 15
+#define TAM 5
 
-void exibir(int vet[15]){
+void exibir(int vet[TAM]){
   int i;
-  for(i=0; i<15; i++)
+  for(i=0; i<TAM; i++)
     printf("%d ", vet[i]);
     putchar('\n');
 }
 
-void gerar(int vet[15]){
+void gerar(int vet[TAM]){
   int i;
-  for(i=0; i<15;i++)
+  for(i=0; i<TAM;i++)
       vet[i] = 1 + rand()%TAM;
   puts("Esse é seu vetor");
   exibir(vet);
 }
 
-int pares(int vet[15]){
+int pares(int vet[TAM]){
     int i,cont;
 
-    for(i=0,cont=0; i<15; i++){
+    for(i=0,cont=0; i<TAM; i++){
         if(vet[i]%2==0)
             cont++;
     }
     printf("A quantidade de números pares é %d\n", cont);
 }
 
-int soma_impares(int vet[15]){
+int soma_impares(int vet[TAM]){
     int i,soma;
 
-    for(i=0,soma=0; i<15; i++){
+    for(i=0,soma=0; i<TAM; i++){
         if(vet[i]%2!=0)
             soma+=vet[i];
     }
     printf("A soma dos números impares é %d\n", soma);
 }
 
-int media(int vet[15]){
+int media(int vet[TAM]){
     int i,cont;
     float media;
 
-    for(i=0,media=0.0; i<15; i++){
+    for(i=0,media=0.0; i<TAM; i++){
         media+=vet[i];
     }
     media/=15;
-    for(i=0,cont=0; i<15; i++){
+    for(i=0,cont=0; i<TAM; i++){
         if(vet[i]>media)
         cont++;
     }
     printf("Existem %d números acima da média\n", cont);
 }
 
-int maior(int vet[15]){
+int maior(int vet[TAM]){
     int i,maior;
 
-    for(i=0,maior=0; i<15; i++){
+    for(i=0,maior=0; i<TAM; i++){
         if(vet[i]>maior)
             maior=vet[i];
     }
     printf("O maior valor do vetor é %d\n", maior);
 }
 
-int v_cons(int vet[15]){
+int v_cons(int vet[TAM]){
     int i=0;
     int m_int = abs(vet[i]-vet[i+1]);
-    for(i=1; i<15; i++){
+    for(i=1; i<TAM; i++){
         if(abs(vet[i]-vet[i+1])>m_int)
             m_int=abs(vet[i]-vet[i+1]);
     }
     printf("A maior diferença em valor absoluto é %d\n", m_int);
 }
 
-void ordenar(int vet[15]){
-    int i,aux;
+void ordenar(int vet[TAM]){
+    int i,j,aux;
 
-    for(i=0; i<14; i++){
-        if(vet[i]>vet[i+1]){
-            aux=vet[i];
-            vet[i]=vet[i+1];
-            vet[i+1]=aux;
-        i=-1;
+    for(i=0; i<TAM; i++){
+        for(j=0; j<TAM; j++){
+            if(vet[i]<vet[j]){
+                aux=vet[i];
+                vet[i]=vet[j];
+                vet[j]=aux;
+            }
         }
     }
     exibir(vet);
 }
 
-void ter_maior(int vet[15]){
+void ter_maior(int vet[TAM]){
     int i,cont,aux;
 
-    for(i=0; i<14; i++){
-        if(vet[i]>vet[i+1]){
-            aux=vet[i];
-            vet[i]=vet[i+1];
-            vet[i+1]=aux;
-        i=-1;
-        }
-     }
-     exibir(vet);
-      for(i=0,cont=1; i<15; i++){
+     ordenar(vet);
+      for(i=0,cont=1; i<TAM; i++){
         if(vet[i+1]>vet[i])
           cont++;
-      if(cont==3)
+      if(cont==3){
         printf("O terceiro maior é %d\n", vet[i+1]);
+        break;
+      }
     }
 }
 
-void subs(int vet[15]){
+void subs(int vet[TAM]){
   int i,a;
 
   exibir(vet);
-  for(a=0; a<15; a++){
-    for(i=a+1; i<15; i++){
+  for(a=0; a<TAM; a++){
+    for(i=a+1; i<TAM; i++){
       if(vet[a]==vet[i])
         vet[i] = 1 + rand()%TAM;
     }
@@ -127,7 +122,7 @@ void subs(int vet[15]){
   exibir(vet);
 }
 
-void menu(int vet[15]){
+void menu(int vet[TAM]){
   int escolha,i,ver=0;
   do{
   puts("MENU");
@@ -158,7 +153,7 @@ void menu(int vet[15]){
 }
 
 int main(){
-    int vet[15];
+    int vet[TAM];
     srand(time(0));
 
     gerar(vet);
