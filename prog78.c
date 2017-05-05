3c46394d695a40d3e954819cb78bd2e0e0424a41
NoobManBr <mayconbj15@gmail.com>
2017-05-05 12:58:48 -0300

    Update prog78.c

--- prog78.c
+++ prog78.c
@@ -4,11 +4,35 @@ void calc(int *v, int num, int *xmin, int *xmax);
 
 int main(){
     int v[10];
-    int xmin,xmax,num,i;
+
+    int num,i;
     srand(time(0));
     for(i=0; i<10; i++){
         v[i]=rand()%10 +1;
         printf("%d ", v[i]);
     }
+    putchar('\n');
+    int *xmin = &v[0], *xmax = &v[0];
+    //printf("%d - %d\n", *xmin,*xmax);
+    puts("Digite o seu limite de números");
+    scanf("%d", &num);
+    calc(v,num,*xmin,*xmax);
+}
 
+void calc(int *v, int num, int *xmin, int *xmax){
+    int i,j;
+
+    for(i=0;i<num;i++){ //maior número
+        for(j=0; j<num; j++)
+            if(v[j]>v[i])
+                xmax=&v[j];
+    }
+    for(i=0;i<num;i++){ //menor número
+        for(j=0; j<num; j++)
+            if(v[j]<v[i])
+                xmin=&v[j];
+    }
+    //printf("%d - %d\n", *xmax, *xmin);
+    printf("O maior dos %d números é %d\n", num,*xmax);
+    printf("O menor dos %d números é %d\n", num,*xmin);
 }
